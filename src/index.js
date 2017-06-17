60288bfd8ab00760e119d22324babd50c917e830
harshpatel991 <hpatel90@illinois.edu>
2017-06-17 13:43:13 -0500

    Add 404 route and page shell

--- src/index.js
+++ src/index.js
@@ -6,13 +6,18 @@ import "bootstrap/dist/css/bootstrap.css";
 import {BrowserRouter, Route} from "react-router-dom";
 import LoginComponent from "./pages/LoginComponent";
 import HomeComponent from "./pages/HomeComponent";
+import NotFoundComponent from "./pages/NotFoundComponent";
+import {Switch} from "react-router";
 
 
 ReactDOM.render(
     <BrowserRouter>
         <App>
+            <Switch>
             <Route exact path='/' component={HomeComponent}/>
-            <Route path='/login' component={LoginComponent}/>
+            <Route exact path='/login' component={LoginComponent}/>
+            <Route path='*' component={NotFoundComponent}/>
+            </Switch>
         </App>
     </BrowserRouter>,
     document.getElementById('root')
---
+++ src/pages/NotFoundComponent.js
@@ -0,0 +1,19 @@
+import React, {Component} from "react";
+import {Col, Container, Row} from "reactstrap";
+
+class NotFoundComponent extends Component {
+    render() {
+        return (
+            <Container fluid="true" >
+                <Row>
+                    <Col className="text-center hero-background">
+                        <img className="mx-auto d-block" src="/images/swap-icon.png" width="100px"/>
+                        <h1>404 Not Found</h1>
+                    </Col>
+                </Row>
+            </Container>
+        );
+    }
+}
+
+export default NotFoundComponent;
